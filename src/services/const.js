2f841950bdad730558bd907c793739efa65faf9f
Kowalska Marta <kamarta0991@gmail.com>
2023-11-19 19:06:45 +0100

    update const.js

--- src/services/const.js
+++ src/services/const.js
@@ -1,17 +1,2 @@
 export const API_KEY = process.env.REACT_APP_API_KEY;
 export const API_BASE_URL = 'https://api.themoviedb.org/3';
-
-export const API_ENDPOINTS = {
-  TRENDING_MOVIES: '/trending/movie/day',
-  MOVIE_DETAILS: '/movie/',
-  SEARCH_MOVIE: '/search/movie',
-};
-
-export const STATUS = {
-  IDLE: 'idle',
-  PENDING: 'pending',
-  RESOLVED: 'resolved',
-  REJECTED: 'rejected',
-};
-
-export const ERROR_MESSAGE = 'Oops! Sorry, something went wrong...';
